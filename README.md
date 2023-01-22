aee22cbe655b7173ebdcd6dd6cf13fd6febc8c94
Jsheen98 <jsheendev@gmail.com>
2023-01-22 12:34:04 -0700

    Added complete README file, as well as completed card content

---
+++ README.md
@@ -0,0 +1,50 @@
+# Challenge 6: Weather Dashboard
+
+## ✏️ Description 
+
+With this project, I wanted to create a useful resource for travelers to view the current day's weather, as well as the next 5 days. The user will input a search (it must be a valid location), and then press the search. The previously searched location(s) will be persisted to the page via local storage, and will be clickable. Weather a previous location is tapped, or a new location is searched, the weather cards will be dynamically created via JavaScript. These cards include the following information: Location, Date, Icon Representation of the current weather, Temperature, Wind Speed, and Humidity. Each new search will generate new content.
+
+## User Story
+
+* AS A traveler
+* I WANT to see the weather outlook for multiple cities
+* SO THAT I can plan a trip accordingly
+
+
+## Acceptance Criteria
+
+* GIVEN a weather dashboard with form inputs
+* WHEN I search for a city
+* THEN I am presented with current and future conditions for that city and that city is added to the searc history
+* WHEN I view current weather conditions for that city
+* THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+* WHEN I view future weather conditions for that city
+* THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+* WHEN I click on a city in the search history
+* THEN I am again presented with current and future conditions for that city
+
+## 🖥️ Technologies Used
+
+* JavaScript/HTML/CSS
+* jQuery
+* Open Weather Map API (for geolocation and forecast)
+
+
+## ⚙️ Installation
+
+N/A
+
+## 🌐 Deployed Application
+
+The deployed application can be found at this link:
+https://jsheen98.github.io/coding-bootcamp-challenge-weather-dashboard/
+
+## 📸 Assets
+
+The following images represent the website's appearance:
+
+![Weather Dashboard](./assets/Images/weather-dashboard-ex.png)
+
+## 📜 License
+
+Please refer to the LICENSE in the repo
--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,25 +2,19 @@ var APIkey = "1be5c11c833eea2a7b4c716a205a1674";
 var city = $("#location-name");
 var previousSearchContainer = $("#previous-search-container");
 var cities = JSON.parse(localStorage.getItem("location")) || [];
-var forecastDiv = $('#five-day-forecast')
-var forecastContainer = $('#forecast-container')
-var currentDayForecastSection = $('#current-day-forecast-section')
-var forecastTitle = $('#forecast-title')
-var cardContainer = $('#card-container')
-var li = $('<li>')
+var forecastDiv = $("#five-day-forecast");
+var currentDayForecastSection = $("#current-day-forecast-section");
+var forecastTitle = $("#forecast-title");
+var li = $("<li>");
 
 function fetchLocation(event) {
-    // if statement if event came from city button or if it came from search // else place = city.val()
-    // var place = event.target.innerText
+  // TODO: if statement: if event came from city button or if it came from search // else place = city.val()
+  // var place = event.target.innerText
+  // var locationQueryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + place + "&appid=" + APIkey
 
-    // var locationQueryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + place + "&appid=" + APIkey
-        var locationQueryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city.val() + "&appid=" + APIkey
-   
-        
-        
-    
+  var locationQueryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city.val() + "&appid=" + APIkey;
 
-
+  // fetches data from above API searching the location name, and variables are created for lat and lon to be used in the next fetch function
   fetch(locationQueryURL)
     .then(function (response) {
       return response.json();
@@ -35,64 +29,67 @@ function fetchLocation(event) {
         var lon = data[0].lon;
 
         saveLocationAndCreateListItem();
-
-        console.log(lon);
-        console.log(lat);
-        fetchWeather(lat, lon)
-        
+        fetchWeather(lat, lon);
       }
     });
 }
 
 function fetchWeather(lat, lon) {
-  var weatherQueryURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIkey;
+  var weatherQueryURL =
+    "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIkey + "&units=imperial";
 
+    // fetches the data from the above API, uses lat and lon variables from the fetchLocation function
   fetch(weatherQueryURL)
-  .then(function(response){
-    return response.json()
-  }) 
-  .then(function(data){
-    var fiveDayContainer = ''
-    var currentDayForecastCard = ''
-    var fiveDayTitleContainer = ''
-    var currentTimestamp = data.list[1].dt
-    var currentDate = new Date (currentTimestamp * 1000)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var fiveDayContainer = "";
+      var currentDayForecastCard = "";
+      var fiveDayTitleContainer = "";
+      var currentTimestamp = data.list[1].dt;
+      var currentDate = new Date(currentTimestamp * 1000);
 
-    currentDayForecastCard += `
+      // dynamically creates the current day forecast card
+      currentDayForecastCard += `
     <section id="current-day-forecast">
-      <h3>${data.city.name} (${currentDate.toLocaleDateString('default')}) <img src='https://openweathermap.org/img/w/${data.list[1].weather[0].icon}.png'></h3>
+      <h3>
+      ${data.city.name} (${currentDate.toLocaleDateString("default")}) 
+      <img src='https://openweathermap.org/img/w/${data.list[1].weather[0].icon}.png'>
+      </h3>
       <p><span class="bold">Temp:</span> ${data.list[1].main.temp}°F</p>
-      <p><span class="bold">Wind:</span> ${data.list[1].wind.speed}</p>
+      <p><span class="bold">Wind:</span> ${data.list[1].wind.speed} MPH</p>
       <p><span class="bold">Humidity:</span> ${data.list[1].main.humidity}%</p>
-    </section>`
-    
-    fiveDayTitleContainer += `<h3>Next Five Days: <h3>`
-  
-    for (var i = 1; i < 6; i++){
-        var timestampFiveDay = data.list[i*7].dt
-        var date = new Date(timestampFiveDay * 1000)
-        fiveDayContainer += 
-        `<div class="forecast-card">
-                <p><span class="bold">${date.toLocaleDateString('default')}<span class="bold"></p>
-                <p><img src='https://openweathermap.org/img/w/${data.list[i*7].weather[0].icon}.png'></p>
-                <p><span class="bold">Temp:</span> ${data.list[i*7].main.temp}°F</p>
-                <p><span class="bold">Wind:</span> ${data.list[i*7].wind.speed}</p>
-                <p><span class="bold">Humidity:</span> ${data.list[i*7].main.humidity}%</p>
-            </div>`
-    }
-    forecastDiv.html(fiveDayContainer)
-    currentDayForecastSection.html(currentDayForecastCard)
-    forecastTitle.html(fiveDayTitleContainer)
-    
-  })
-    
+    </section>`;
+
+      // dynamically creates the next five days title
+      fiveDayTitleContainer += `<h3>Next Five Days: <h3>`;
+
+      // dynamically creates the next five days cards
+      for (var i = 1; i < 6; i++) {
+        var timestampFiveDay = data.list[i * 7].dt;
+        var date = new Date(timestampFiveDay * 1000);
+        fiveDayContainer += `
+            <div class="forecast-card">
+                <p><span class="bold">${date.toLocaleDateString("default")}<span class="bold"></p>
+                <p><img src='https://openweathermap.org/img/w/${data.list[i * 7].weather[0].icon}.png'></p>
+                <p><span class="bold">Temp:</span> ${data.list[i * 7].main.temp}°F</p>
+                <p><span class="bold">Wind:</span> ${data.list[i * 7].wind.speed} MPH</p>
+                <p><span class="bold">Humidity:</span> ${data.list[i * 7].main.humidity}%</p>
+            </div>`;
+      }
+      // appends the cards to the document
+      forecastDiv.html(fiveDayContainer);
+      currentDayForecastSection.html(currentDayForecastCard);
+      forecastTitle.html(fiveDayTitleContainer);
+    });
 }
 
-
+// function to save locations to localStorage
 function saveLocationAndCreateListItem() {
   cities.push(city.val());
-  localStorage.setItem('location', JSON.stringify(cities))
-  var userInput = city.val()
+  localStorage.setItem("location", JSON.stringify(cities));
+  var userInput = city.val();
   // Goes through and makes sure the user input capitalizes all first letters on each word (i.e. 'new york' becomes 'New York')
   var capitalize = userInput.split(" ");
   for (var i = 0; i < capitalize.length; i++) {
@@ -104,6 +101,7 @@ function saveLocationAndCreateListItem() {
   city.val("");
 }
 
+// function that loads locations to the page via local storage
 function loadItemsOnPage() {
   // Goes through and makes sure the user input capitalizes all first letters on each word (i.e. 'new york' becomes 'New York')
   if (cities.length == 0) {
@@ -116,28 +114,24 @@ function loadItemsOnPage() {
           capitalize[i].charAt(0).toUpperCase() + capitalize[i].slice(1);
       }
       var userInputFinal = capitalize.join(" ");
-      li = $('<li>')
-      li.text(userInputFinal) // jquery version of innerText
-      li.click(fetchLocation) // click method for jquery
-      $("#previous-searches").prepend(li)
-      
-    //   $("#previous-searches").prepend("<li>" + userInputFinal + "</li>");
+      li = $("<li>");
+      li.text(userInputFinal);
+      // li.click(fetchLocation); TODO
+      $("#previous-searches").prepend(li);
     }
   }
 }
 
-
+// submit button for search bar
 $("#submit-btn").click(function (event) {
   event.preventDefault();
+  // If city.val is blank send alert, else, run the call the function
+  if (!city.val()) {
+    alert("Please enter a valid search")
+  } else {
   fetchLocation();
-//   console.log(event.target.tagName)
-//   console.log(event.target)
-//   if (!city.val()) {
-//     alert("Please enter a valid search");
-//   } else {
-    
-//   }
+  }
 });
 
+// function call for local storage functionality
 loadItemsOnPage();
-
